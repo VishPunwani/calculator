1aa754ab6235ff1c3fd1fdb4927c3160ec12512a
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-03-22 23:35:22 -0700

    compuation and all clear works

--- index.js
+++ index.js
@@ -31,7 +31,29 @@ class Calculator {
     }
 
     compute(){
-
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+         if (isNaN(prev) || isNaN(current)) return
+         switch (this.operation) {
+         case '+':
+             computation = prev + current
+             break
+        case '-':
+            computation = prev - current
+            break
+        case '*':
+            computation = prev * current
+            break
+      case '÷':
+            computation = prev / current
+            break
+      default:
+        return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
 
     updateDisplay(){
@@ -66,9 +88,13 @@ operationButtons.forEach(button => {
     })
   })
 
-  equalsButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      calculator.compute()
-      calculator.updateDisplay()
-    })
+  equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
   })
+
+  allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+  })
+  
